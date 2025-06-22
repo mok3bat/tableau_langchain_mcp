6ced9538744f0c6918464a50a99be336bffebf37
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-22 20:50:41 +0300

    Check

--- main.py
+++ main.py
@@ -1,24 +1,12 @@
 # main.py
 
 import os
-# from tools import mcp  # ✅ Must import tools to run @mcp.tool decorators
-import asyncio
-
-# tools.py
-from mcp.server.fastmcp import FastMCP
-
-
-# Create MCP using that app
-mcp = FastMCP(host="0.0.0.0", 
-            port=os.getenv("PORT", 8000))
-
-# ✅ Define your tools using the MCP instance
-@mcp.tool()
-def say_hi_world() -> str:
-    return "Hello from Railway MCP!"
+from tools import mcp, say_hi_world  # ✅ Must import tools to run @mcp.tool decorators
 
 if __name__ == "__main__":
     # Could also use 'sse' transport, host="0.0.0.0" required for Cloud Run.
-    asyncio.run(
-        mcp.run_sse_async()
-    )
+    
+    mcp.register_tool(say_hi_world)
+
+    # Expose ASGI app
+    fastmcp_asgi_app = mcp.asgi()
