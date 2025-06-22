2b9eba9fa5655296d23dcdd5f16aab5d2f120987
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-22 21:43:35 +0300

    Check

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ async def root():
     return {"message": "Hello World"}
 
 # ✅ Used only for local testing
-if __name__ == "__main__":
-    import uvicorn
-    import os
-    port = int(os.getenv("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+#if __name__ == "__main__":
+import uvicorn
+import os
+port = int(os.getenv("PORT", 8000))
+uvicorn.run(app, host="0.0.0.0", port=port)
