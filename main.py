9f218cd6189ff2ff541156fca806156bd026f4c7
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-22 21:22:24 +0300

    Check

--- main.py
+++ main.py
@@ -8,10 +8,3 @@ mcp = FastMCP(app=app)
 def say_hi_world() -> str:
     return "Hi World from MCP!"
 
-
-import os
-import uvicorn
-
-
-port = int(os.getenv("PORT", 8000))
-uvicorn.run(app, host="0.0.0.0", port=port)
