4afbf4157d9604f7cf4ed5e287368bda0e6ac4c8
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-22 19:31:33 +0300

    Update Requirements

--- main.py
+++ main.py
@@ -7,9 +7,5 @@ import asyncio
 if __name__ == "__main__":
     # Could also use 'sse' transport, host="0.0.0.0" required for Cloud Run.
     asyncio.run(
-        mcp.run_sse_async(
-            transport="streamable-http", 
-            host="0.0.0.0", 
-            port=os.getenv("PORT", 8000),
-        )
+        mcp.run_sse_async()
     )
