cd252000df4c8ac57296ab9ef25c9a4798c1ade0
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-22 19:43:43 +0300

    Update Requirements

--- tools.py
+++ tools.py
@@ -1,9 +1,11 @@
 # tools.py
 from mcp.server.fastmcp import FastMCP
+import os
 
 
 # Create MCP using that app
-mcp = FastMCP()
+mcp = FastMCP(host="0.0.0.0", 
+            port=os.getenv("PORT", 8000))
 
 # ✅ Define your tools using the MCP instance
 @mcp.tool()
