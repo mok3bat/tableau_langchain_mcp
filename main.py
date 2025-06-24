37c6c4eb7be78cfd2175fe8502fe9aa8907ce2d6
mok3bat <45605757+mok3bat@users.noreply.github.com>
2025-06-24 17:58:40 +0300

    TestNew

--- main.py
+++ main.py
@@ -1,21 +1,25 @@
-from mcp.server.fastmcp import FastMCP
+import contextlib
 from fastapi import FastAPI
-
-headers= {"Accept": "application/json, text/event-stream"}
-mcp = FastMCP("strava", stateless_http=True, headers=headers, host="0.0.0.0", port=8000)
-
-@mcp.tool()
-def get_activities():
-    print('hello')
-    return {
-        "content": [
-            { "type": "text", "text": "✅ Hello from deployed MCP!" }
-        ]
-    }
+from echo_server import mcp as echo_mcp
+# from math_server import mcp as math_mcp
+import os
 
 
-app = FastAPI(title="Strava")
-app.mount("/mcp")
+# Create a combined lifespan to manage both session managers
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with contextlib.AsyncExitStack() as stack:
+        await stack.enter_async_context(echo_mcp.session_manager.run())
+        #await stack.enter_async_context(math_mcp.session_manager.run())
+        yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.mount("/echo", echo_mcp.streamable_http_app())
+#app.mount("/math", math_mcp.streamable_http_app())
+
+PORT = os.environ.get("PORT", 8000)
 
 if __name__ == "__main__":
-    mcp.run(transport='streamable-http')
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
